6b7508d8d02351ad6c43daebf63c5fdbb97cd3de
Julius Klinzer <julius.klinzer@outlook.de>
2026-05-26 12:39:43 +0200

    Add Firmengolf Events plugin scaffold

---
+++ wordpress/wp-content/plugins/firmengolf-events/firmengolf-events.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Firmengolf Events
+ * Description: Custom event marketplace and partner portal for Firmengolf.
+ * Version: 0.1.0
+ * Author: Firmengolf
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
